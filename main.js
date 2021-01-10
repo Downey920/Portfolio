b8bd7a9d4dfcb7402c5374a29e5fc4e70eb1ccb9
Downey920 <skekfl12@naver.com>
2021-01-10 20:46:24 +0900

    JS : navBar ToggleBtn

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector(".navBar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -8,4 +9,11 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navBar--dark");
   }
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navBar__toggleBtn");
+const navbarMenu = document.querySelector(".navBar__menu");
+navbarToggleBtn.addEventListener("click", () => {
+  navbar.classList.toggle("open");
 });
