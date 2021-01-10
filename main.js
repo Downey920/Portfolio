af84e94c63113c052eb733158ca2a4c35b5ae91c
Downey920 <skekfl12@naver.com>
2021-01-11 00:39:58 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -34,6 +34,13 @@ homeBtn.addEventListener("click", event => {
   scrollIntoView(".contact");
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const homeContainer = document.querySelector(".homeContainer");
+const homeHeight = homeContainer.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
